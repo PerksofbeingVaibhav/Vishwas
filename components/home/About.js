1eb3a306318aa8ff9cb927520653db5cfa1cae5f
Vaibhav Chaudhary <83056597+PerksofbeingVaibhav@users.noreply.github.com>
2022-11-13 15:49:05 +0530

    third commit

---
+++ components/home/About.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div
+      className="flex flex-col w-full items-center mt-10 md:mt-14 "
+      id="about"
+    >
+      <p className="text-4xl font-bold">About Us</p>
+      <p className="text-gray-500 text-xl w-full md:w-8/12 text-center my-4">
+      Department of Empowerment of Persons with Disabilities (Divyangjan) under Ministry of Social Justice & Empowerment was set up in May 2012 with the aims to facilitate empowerment and inclusion of the persons with disabilities and acts as a nodal agency to look after all development agenda of Persons with Disabilities (Divyangjan).Empowerment of persons with disabilities is an inter-disciplinary process, covering various aspects namely, prevention, early detection, intervention, education, health, vocational training, rehabilitation and social integration.
+        <br />
+       Our mission is to build an inclusive society in which equal opportunities are provided for the growth and development of Persons with Disabilities so that they can lead productive, safe and dignified lives.
+        <br />
+        Our future plan is to collaborate with different NGOs, old age homes so
+        more benefit of the society. Also, we will combine with mess of all
+        over Kota city to reduce the wastage of food and use it for a good cause as much
+        as possible.
+      </p>
+    </div>
+  );
+}
---
+++ components/home/Hero.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function Hero() {
+  return (
+    <div>
+      <div className="container mx-auto flex flex-col items-center py-12 sm:py-24">
+        <div className="w-11/12 sm:w-2/3 lg:flex justify-center items-center flex-col  mb-5 sm:mb-10">
+          <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl text-center text-gray-800 font-black leading-7 md:leading-10">
+            Help Because You Can <br/>
+            <span className="text-indigo-700">{" "}Nourish your Soul{" "}</span>
+          </h1>
+          <p className="mt-5 sm:mt-10 lg:w-10/12 text-gray-400 font-normal text-center text-sm sm:text-lg">
+            <strong>Team Vishwas with a vision for providing platform such as  PWAs and offline Android/iOS apps, highly accessible to all types of disable persons that provides details of various schemes by Govt. of India </strong>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
